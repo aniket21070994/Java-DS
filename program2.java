12119e09191beb8b9fef3cedb355fae972e65955
aniket21070994 <aniket.mankar001@gmail.com>
2024-03-05 20:38:05 +0530

    second program

---
+++ program2.java
@@ -0,0 +1,12 @@
+class program2{
+    
+     public static void main (String[] args) {
+        int counter=0;
+        
+        while(counter<=10)
+        {
+        System.out.println(counter);
+        counter++;
+       }
+     }
+}
